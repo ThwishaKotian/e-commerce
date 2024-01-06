8a6b46f7bdabd8e7a09e33e86a5e97f9e7db068c
ThwishaKotian <thwishakotian@gmail.com>
2024-01-07 03:15:48 +0530

    Navbar implemented

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './Navbar.css'
+import logo from '../Assets/logo.png'
+import cart_icon from '../Assets/cart_icon.png'
 
 const Navbar = () => {
-  return (
-    <div className='navbar'>
-        
-    </div>
-  )
+    const [menu, setMenu] = useState('shop')
+    return (
+        <div className='navbar'>
+            <div className="nav-logo">
+                <img src={logo} alt="" />
+                <p>SHOP SPHERE</p>
+            </div>
+            <ul className="nav-menu">
+                <li onClick={() => setMenu('shop')}>Shop{menu === 'shop' ? <hr/> : <></>}</li>
+                <li onClick={() => setMenu('men')}>Men{menu === 'men' ? <hr/> : <></>}</li>
+                <li onClick={() => setMenu('women')}>Women{menu === 'women' ? <hr/> : <></>}</li>
+                <li onClick={() => setMenu('kids')}>Kids{menu === 'kids' ? <hr/> : <></>}</li>
+            </ul>
+            <div className="nav-login-cart">
+                <button>Login</button>
+                <img src={cart_icon}></img>
+                <div className="nav-cart-count">0</div>
+            </div>
+        </div>
+    )
 }
 
 export default Navbar
