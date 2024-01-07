13477fb2bc47fb08895402bbc6bf2fd2b853d661
ThwishaKotian <thwishakotian@gmail.com>
2024-01-07 15:33:47 +0530

    Footer added to all sections

--- src/App.js
+++ src/App.js
@@ -7,23 +7,25 @@ import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product';
 import LoginSignup from './Pages/LoginSignup';
 import Cart from './Pages/Cart';
+import Footer from './Components/Footer/Footer';
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Navbar />
-<Routes>
-  <Route path='/' element={<Shop />}/>
-  <Route path='/mens' element={<ShopCategory category='mens'/>}/>
-  <Route path='/womens' element={<ShopCategory category='womens'/>}/>
-  <Route path='/kids' element={<ShopCategory category='kids'/>}/>
-  <Route path='/product' element={<Product />}>
-    <Route path=':productId' element={<Product />}/>
-  </Route>
-  <Route path='/cart' element={<Cart />}/>
-  <Route path='/login' element={<LoginSignup />}/>
-</Routes>
+        <Routes>
+          <Route path='/' element={<Shop />} />
+          <Route path='/mens' element={<ShopCategory category='mens' />} />
+          <Route path='/womens' element={<ShopCategory category='womens' />} />
+          <Route path='/kids' element={<ShopCategory category='kids' />} />
+          <Route path='/product' element={<Product />}>
+            <Route path=':productId' element={<Product />} />
+          </Route>
+          <Route path='/cart' element={<Cart />} />
+          <Route path='/login' element={<LoginSignup />} />
+        </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
