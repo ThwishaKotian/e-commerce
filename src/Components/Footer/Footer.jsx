24ef99b62b0ddfeca2a29f35d986b73f81041d03
ThwishaKotian <thwishakotian@gmail.com>
2024-01-08 16:16:09 +0530

    Title fix

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -10,7 +10,7 @@ const Footer = () => {
         <div className='footer'>
             <div className="footer-logo">
                 <img src={footer_logo} />
-                <p>SHOP SPHERE</p>
+                <p>SHOP HOP</p>
             </div>
             <ul className="footer-links">
                 <li>Company</li>
